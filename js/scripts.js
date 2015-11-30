feb0e7ea4998593dcd90a934fe0ddd07be66d0f1
David Williams <davidvwilliams@yahoo.com>
2015-11-30 16:48:39 -0500

    add player paddle control and bounderies

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+var upArrow = 38;
+var downArrow = 40;
+var spaceBar = 32;
+
 window.onload = function () {
 	var pongCanvas = document.getElementById('pong_canvas');
 	if(pongCanvas && pongCanvas.getContext) {
@@ -9,7 +13,8 @@ window.onload = function () {
 			pongCanvas.width = 1024;
 			pongCanvas.height = 640;
 
-			// Set rectangle color
+			function drawBoard(){
+							// Set rectangle color
 			ctx.fillStyle = "#222";
 			ctx.fillRect(50,50, 924, 540);
 			//Add stroke to rectangle
@@ -24,16 +29,23 @@ window.onload = function () {
 			ctx.moveTo(512,60);
 			ctx.lineTo(512, 580);
 			ctx.stroke();
+			}
 
-			// Draw  player paddle, left side
-			// ctx.fillStyle = "blue";
-			// ctx.fillRect(75,75,20,125)
-			player.render(ctx);
-			computer.render(ctx);
-			pongBall.draw(ctx);
+			// Draw paddles and ball	
+			function step() {
+				ctx.save();
+				ctx.clearRect(0, 0, pongCanvas.width, pongCanvas.height);
+				drawBoard();
+				player.render(ctx);
+				computer.render(ctx);
+				pongBall.draw(ctx);	
+				player.move();
+				animate(step);	
+				ctx.restore();			
+			}		
+			step();
 		}
 	}
-
 }
 
 function Paddle(x,y,width,height,paddleColor) {
@@ -42,7 +54,34 @@ function Paddle(x,y,width,height,paddleColor) {
 	this.width = width || 20;
 	this.height = height || 125;
 	this.color = paddleColor || "blue";
+	this.speed = 10;
+	this.move = function(){
+		var boardTop = 50;
+		var boardBottom = 460;
+		// change this.y position by the speed
+		if (this.y == boardTop) {
+			alert("top");
+			this.y += 10;			
+		} else if (this.y == boardBottom) {
+			alert("bottom");
+			this.y -= 10;			
+		}
+		// check to make sure new position isn't past board edges or else move it back to board edge
+
+	  //console.log(player.y);
+	}
 }
+// keydown make sure is arrow key up/down
+// on keyup reset speed y to 0
+var pressArrowKey = window.addEventListener("keydown", function(e) {
+	e.preventDefault();
+	console.log(player.y);
+	if (e.keyCode == downArrow) {
+		player.y += player.speed;
+	} else if (e.keyCode == upArrow) {
+		player.y -= player.speed;
+	}
+}, false);
 
 Paddle.prototype.render = function (c) {
 	c.fillStyle = this.color;
@@ -51,8 +90,9 @@ Paddle.prototype.render = function (c) {
 	c.stroke();
 }
 
-var player = new Paddle(75,100,20,125,"red");
-var computer = new Paddle(930,400,20,125,"red");
+var player = new Paddle(75,250,20,125,"red");
+var computer = new Paddle(930,250,20,125,"red");
+var pongBall = new Ball(600,300,15, "lightgray");
 
 function Ball (x, y, radius, color) {
 	this.x = x;
@@ -68,8 +108,15 @@ function Ball (x, y, radius, color) {
 	}
 }
 
-var pongBall = new Ball(600,300,15, "lightgray");
 
+
+// animate
+var animate = window.requestAnimationFrame ||
+window.webkitRequestAnimationFrame ||
+window.mozRequestAnimationFrame    ||
+window.oRequestAnimationFrame      || 
+window.msRequestAnimationFrame     ||
+function(callback) { window.setTimeout(callback, 1000/60) };
 
 
 
